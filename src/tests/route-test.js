242fdd3d18cf0d16f8318bee038f1d5450be1cdc
Phat Ma <phat.ma@hotmail.co.uk>
2018-04-25 19:19:05 +0100

    add home route test

--- src/tests/route-test.js
+++ src/tests/route-test.js
@@ -1,8 +1,18 @@
-const test = require('tape');
-const request = require('supertest');
-const app = require('../app');
+const test = require("tape");
+const request = require("supertest");
+const app = require("../app");
 
-test('Test tape is running', t => {
-    t.equal(1,1, 'Tape is working');
-    t.end();
-})
+test("Test tape is running", t => {
+  t.equal(1, 1, "Tape is working");
+  t.end();
+});
+
+test("Home route access", t => {
+  request(app)
+    .get("/")
+    .expect(200)
+    .end((err, res) => {
+      t.error(err);
+      t.end();
+    });
+});

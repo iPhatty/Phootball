ee7350fe71adb1659fc698c829bef11e1064560a
Phat Ma <phat.ma@hotmail.co.uk>
2018-04-26 18:20:04 +0100

    express handlebars set up

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
+// require controllers
+const controllers = require('./controllers/index');
+
+// Set up view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    defaultLayout: 'main',
+    // helpers,
+  }),
+);
+
+// Server setup
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(controllers);
 // Home Route
 app.get('/', (req, res) => {
   res.send('Hello');

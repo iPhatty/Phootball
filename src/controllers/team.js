16692ad75fb5c1ac06e0d88cae1fbfedaea45ad0
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-20 02:47:25 +0100

    render fixture information into handlebar Relates #8

--- src/controllers/team.js
+++ src/controllers/team.js
@@ -2,23 +2,26 @@ const premierInfo = require('../model/premier_info.js');
 
 exports.get = (req, res, next) => {
   const { id } = req.params;
-  // API call for past 7 days fixtures
-  premierInfo(`http://api.football-data.org/v1/teams/${id}/fixtures?timeFrame=p7`, (err1, pastFixtures) => {
-    if (err1) {
-      next(err1);
-    }
-    // Second API call for next 7 days fixtures
-    premierInfo(`http://api.football-data.org/v1/teams/${id}/fixtures?timeFrame=n7`, (err2, nextFixtures) => {
-      if (err2) {
-        next(err2);
+
+  premierInfo(`http://api.football-data.org/v1/teams/${id}/`, (err, team) => {
+    // API call for past 7 days fixtures
+    premierInfo(`http://api.football-data.org/v1/teams/${id}/fixtures?timeFrame=p7`, (err1, pastFixtures) => {
+      if (err1) {
+        next(err1);
       }
+      // Second API call for next 7 days fixtures
+      premierInfo(`http://api.football-data.org/v1/teams/${id}/fixtures?timeFrame=n7`, (err2, nextFixtures) => {
+        if (err2) {
+          next(err2);
+        }
 
-      //   console.log(nextFixtures.fixtures[0].status);
+        //   console.log(nextFixtures.fixtures[0].status);
 
-      const notFinished = nextFixtures.fixtures.filter(object => object.status === 'TIMED');
+        const notFinished = nextFixtures.fixtures.filter(object => object.status === 'TIMED');
 
 
-      return res.render('home', { pastFixtures }, { notFinished });
+        return res.render('team', { team, pastFixtures, nextFixtures });
+      });
     });
   });
 };

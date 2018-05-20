a044afddb5c42b80f40f082e21a0f1a6356d1c4b
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-20 14:05:42 +0100

    implement nock to existing tests

--- src/tests/premier_info.test.js
+++ src/tests/premier_info.test.js
@@ -1,7 +1,16 @@
 const test = require('tape'); // eslint-disable-line
 const request = require('supertest'); // eslint-disable-line
 // const app = require('../app');
+const nock = require('nock');
 const premierInfo = require('../model/premier_info.js');
+const leagueTable = require('./mocks/league-table.json');
+
+const leagueNock = () => {
+  // setup mocked backend for a specific end point
+  nock('https://api.football-data.org/v1/competitions/445/leagueTable') // check to see if http or https!!
+    .get('/')
+    .reply(200, leagueTable);
+};
 
 test('Test tape is running', (t) => {
   t.equal(1, 1, 'Tape is working');
@@ -9,6 +18,9 @@ test('Test tape is running', (t) => {
 });
 
 test('Get Premier League info', (t) => {
+  // make nock call for league table
+  leagueNock();
+
   premierInfo('http://api.football-data.org/v1/competitions/445/leagueTable', (err, league) => {
     if (err) {
       t.error(err);
